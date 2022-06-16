d58761a51dd730f53953ec5bb717645462ad0e6b
Amin_Cs <aminnoor.cs@gmail.com>
2022-06-17 02:42:28 +0500

    origin

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
  
-import HomePage from "./homePage";
+ import Layout from "../components/layout/Layout";
+//  import ShopByCateg from "../components/shop-by-category/ShopByCateg";
+//  import TextImgSection from "../components/text-img-section/TextImgSection";
+ import katwalkBag from "../assets/images/designers/katwalk-bag.jpg";
+ import bestSellingProduct from "../assets/images/best-selling/best-selling-product.jpg";
+ import Features from "../components/features/Features";
+ import WebTopSection from "../components/web-top-section/WebTopSection";
  
 import { useEffect } from "react";
 
@@ -7,23 +13,28 @@ export default function Home() {
   useEffect(() => {
     document.querySelector("body").style.direction = 'ltr'
   }, [])
+  const designerData = {
+    title: "designers",
+    text: "Explore our talented and independent designers.",
+    img: katwalkBag,
+    type: "designer"
+  };
+  const bestSellingData = {
+    title: "Best Selling",
+    text: "Our customers' favorite picks",
+    img: bestSellingProduct,
+    type: "product"
+  }
+
+
   return (
 
-    <>
-      <HomePage />
-      {/* <PrivacyPolicy /> */}
-      {/* <FAQ /> */}
-      {/* <About /> */}
-      {/* <ProductDetail /> */}
-      {/* <Checkout /> */}
-      {/* <AllUsedIcons /> */}
-      {/* <Login /> */}
-      {/* <Shop /> */}
-      {/* <Designers /> */}
-      {/* <DesignerDetail /> */}
-
-      {/* admin panel */}
-      {/* <AdminPanel /> */}
-    </>
+    <Layout>
+    <WebTopSection />
+    {/* <ShopByCateg /> */}
+    {/* <TextImgSection data={designerData} />
+    <TextImgSection data={bestSellingData} /> */}
+    <Features />
+  </Layout>
   );
 }
