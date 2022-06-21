efc74e839a934a5be17bb8565eec3b448b20ac89
Amin_Cs <aminnoor.cs@gmail.com>
2022-06-21 12:46:48 +0500

    origi5a

---
+++ components/list-for-content-pages/ListForContentPages.js
@@ -0,0 +1,29 @@
+function ListForContentPages({ data }) {
+  return (
+  <>
+    <h3 className="work-regular text-[20px] pt-[20px] mt-[16px]">
+    {data ? <>{data.title ? data.title : ""}</> : ""}
+  </h3>
+    <ul className="list-disc ml-[15px] mt-[15px]">
+    {data.text ? (
+      <>
+        {data.text.map((val, ind) => {
+          return (
+            <li
+              key={val}
+              className="work-light text-[#6b6e73] text-[15px] leading-[28px] tracking-[0.5px]"
+            >
+              {val}
+            </li>
+          );
+        })}
+      </>
+    ) : (
+      ""
+    )}
+  </ul>
+  </>
+  );
+}
+
+export default ListForContentPages;
