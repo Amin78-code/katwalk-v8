fa44b20796bece8fe3675f1e90ef370d7795eb77
Amin_Cs <aminnoor.cs@gmail.com>
2022-06-30 16:20:08 +0500

    origin6_30f

--- assets/data/wishListProductsArabic.js
+++ assets/data/wishListProductsArabic.js
@@ -5,31 +5,31 @@ import PrintedSleeveAbaya from "../images/products/Printed Sleeve Abaya.jpg";
 import AbayaSetwithSideSlet from "../images/products/Abaya Set with Side Slet.jpg";
 
 const productsArabic = [
-  // {
-  //   img: ContrastPipingTrimAbaya,
-  //   name: "عباية بأكمام تول مطرزة بزهور",
-  //   price: "1,030",
-  // },
-  // {
-  //   img: CurvyFrontLinebuttonAbaya,
-  //   name: "عباية بأشرطة بلون مغاير",
-  //   price: "750",
-  // },
-  // {
-  //   img: AbayaSetwithSideSlet,
-  //   name: "عباية بتطريز ترتر",
-  //   price: "1,300",
-  // },
-  // {
-  //   img: PrintedSleeveAbaya,
-  //   name: "عباية بأزرار أمامية منحنية وشرّابات",
-  //   price: "820",
-  // },
-  // {
-  //   img: PearlEmbroideryAbaya,
-  //   name: "عباية بكشكش جانبي",
-  //   price: "900",
-  // },
+  {
+    img: ContrastPipingTrimAbaya,
+    name: "عباية بأكمام تول مطرزة بزهور",
+    price: "1,030",
+  },
+  {
+    img: CurvyFrontLinebuttonAbaya,
+    name: "عباية بأشرطة بلون مغاير",
+    price: "750",
+  },
+  {
+    img: AbayaSetwithSideSlet,
+    name: "عباية بتطريز ترتر",
+    price: "1,300",
+  },
+  {
+    img: PrintedSleeveAbaya,
+    name: "عباية بأزرار أمامية منحنية وشرّابات",
+    price: "820",
+  },
+  {
+    img: PearlEmbroideryAbaya,
+    name: "عباية بكشكش جانبي",
+    price: "900",
+  },
 ];
 
 export default productsArabic;
--- components/dashboard/wishlist-card/WishListCard.js
+++ components/dashboard/wishlist-card/WishListCard.js
@@ -6,14 +6,14 @@ import TableHeader from "../table-header/TableHeader";
 
 function WishListCard({ data }) {
   return (
-    <div className="w-[32.4%] mb-[0.5rem]">
+    <div className="w-[49.4%] lg:w-[32.4%] mb-[0.5rem]">
       <TitleAndTableCard>
-        <div className="py-[20px] px-[25px]">
+        <div className="py-[10px] sm:py-[20px] px-[10px] sm:px-[25px]">
           <span className="image_container">
             <Image src={data.img} alt="image" />
           </span>
           <div>
-            <h5 className="fwr font-[600] text-[0.875rem] text-[#1b1b28] mt-[23px]">
+            <h5 className="fwr font-[600] text-[0.875rem] text-[#1b1b28] mt-[23px] overflow-hidden overflow-ellipsis whitespace-nowrap">
               {data.name}
             </h5>
             <p className="fwr font-[600] text-[0.875rem] text-[#c53a19]">
@@ -21,7 +21,7 @@ function WishListCard({ data }) {
             </p>
           </div>
         </div>
-        <div className="py-[12px] px-[25px] border-t-[1px] border-[#ebedf2]">
+        <div className="py-[10px] sm:py-[12px] px-[10px] sm:px-[25px] border-t-[1px] border-[#ebedf2]">
           <button className="w-[100%] block light-brown-btn ffr text-[0.875rem] text-[#fff] leading-[40px] tracking-[0.5px] uppercase bg-[#c83e27] px-[15px] mx-auto">
             remove
           </button>
