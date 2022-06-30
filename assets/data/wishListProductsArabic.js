faa8837b018b996dba40a1eb44217a40b26f3f5e
Amin_Cs <aminnoor.cs@gmail.com>
2022-06-30 15:25:02 +0500

    origin6_30b

---
+++ assets/data/wishListProductsArabic.js
@@ -0,0 +1,35 @@
+import ContrastPipingTrimAbaya from "../images/products/Contrast Piping Trim Abaya.jpg";
+import CurvyFrontLinebuttonAbaya from "../images/products/Curvy Front Line button Abaya.jpg";
+import PearlEmbroideryAbaya from "../images/products/Pearl Embroidery Abaya.jpg";
+import PrintedSleeveAbaya from "../images/products/Printed Sleeve Abaya.jpg";
+import AbayaSetwithSideSlet from "../images/products/Abaya Set with Side Slet.jpg";
+
+const products = [
+  {
+    img: ContrastPipingTrimAbaya,
+    name: "عباية بأكمام تول مطرزة بزهور",
+    price: "1,030",
+  },
+  {
+    img: CurvyFrontLinebuttonAbaya,
+    name: "عباية بأشرطة بلون مغاير",
+    price: "750",
+  },
+  {
+    img: AbayaSetwithSideSlet,
+    name: "عباية بتطريز ترتر",
+    price: "1,300",
+  },
+  {
+    img: PrintedSleeveAbaya,
+    name: "عباية بأزرار أمامية منحنية وشرّابات",
+    price: "820",
+  },
+  {
+    img: PearlEmbroideryAbaya,
+    name: "عباية بكشكش جانبي",
+    price: "900",
+  },
+];
+
+export default products;
